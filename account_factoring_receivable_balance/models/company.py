073ee06381ef6363a8d879c736dd9a7e66064f81
David Beal <david.beal@akretion.com>
2023-12-15 09:52:26 +0100

    FIX facto_receiv_bal: pylint string name

--- account_factoring_receivable_balance/models/company.py
+++ account_factoring_receivable_balance/models/company.py
@@ -12,7 +12,7 @@ class ResCompany(models.Model):
 
     factor_config_currency_id = fields.Many2one(
         comodel_name="res.currency",
-        string="Currency",
+        string="Factor Currency",
         help="Use to configure account and journal",
     )
 
